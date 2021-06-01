88892685b6eb8e2a9170c4aabe236580a8bc3f25
Nadya Karaivanova <48494959+nade7o@users.noreply.github.com>
2021-06-01 19:00:11 +0300

    use-gridcomboboxcolumn-with-custom-values

---
+++ GridView/GridViewComboxColumnCustomValue/GridViewComboxColumnCustomValue/GridViewComboxColumnCustomValue/RadForm1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace GridViewComboxColumnCustomValue
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        List<Food> data = new List<Food>();
+
+        public RadForm1()
+        {
+            InitializeComponent();
+            radGridView1.AutoGenerateColumns = false;
+            radGridView1.EditorRequired += radGridView1_EditorRequired;
+            radGridView1.CreateCell += radGridView1_CreateCell;
+            data.Add(new Food(0, "Onion"));
+            data.Add(new Food(1, "Cucumber"));
+            data.Add(new Food(2, "Tomato"));
+            data.Add(new Food(3, "Peach"));
+            data.Add(new Food(4, "Banana"));
+            data.Add(new Food(5, "Grape"));
+
+            GridViewComboBoxColumn comboBoxColumn = new GridViewComboBoxColumn();
+            comboBoxColumn.DataSource = data;
+            comboBoxColumn.DisplayMember = "FoodName";
+            comboBoxColumn.ValueMember = "FoodName";
+            comboBoxColumn.Width = 200;
+            comboBoxColumn.DropDownStyle = Telerik.WinControls.RadDropDownStyle.DropDown;
+            radGridView1.Columns.Add(comboBoxColumn);
+
+            radGridView1.Rows.Add("Onion");
+            
+            radGridView1.CellValueChanged += radGridView1_CellValueChanged;
+        }
+
+        void radGridView1_CreateCell(object sender, GridViewCreateCellEventArgs e)
+        {
+            if (e.CellType == typeof(GridComboBoxCellElement))
+            {
+                e.CellElement = new MyCombBoxCellElement(e.Column as GridViewDataColumn, e.Row);
+            }
+        }
+
+        void radGridView1_CellValueChanged(object sender, GridViewCellEventArgs e)
+        {
+        }
+        
+        void radGridView1_EditorRequired(object sender, EditorRequiredEventArgs e)
+        {
+            if (e.EditorType == typeof(RadDropDownListEditor))
+            {
+                e.EditorType = typeof(MyRadDropDownListEditor);
+            }
+        }
+    }
+
+    public class MyRadDropDownListEditor : RadDropDownListEditor
+    {
+        public override object Value
+        {
+            get
+            {
+                RadDropDownListElement editor = this.EditorElement as RadDropDownListElement;
+                if (editor.SelectedItem != null)
+                {
+                    if (!string.IsNullOrEmpty(editor.ValueMember))
+                    {
+                        return editor.SelectedItem.Value;
+                    }
+
+                    return editor.SelectedItem.Text;
+                }
+
+                return editor.Text;
+            }
+            set
+            {
+                base.Value = value;
+            }
+        }
+    }
+
+    public class MyCombBoxCellElement : GridComboBoxCellElement
+    {
+        public MyCombBoxCellElement(GridViewColumn col, GridRowElement row) : base(col, row)
+        {
+        }
+
+        public override void SetContent()
+        {
+            
+            SetContentCore(this.Value);
+        }
+    }
+
+    public class Food
+    {
+        private int foodID;
+        private string foodName;
+
+        public Food(int foodID, string foodName)
+        {
+            this.foodID = foodID;
+            this.foodName = foodName;
+        }
+
+        public int FoodID
+        {
+            get
+            {
+                return foodID;
+            }
+            set
+            {
+                foodID = value;
+            }
+        }
+
+        public string FoodName
+        {
+            get
+            {
+                return foodName;
+            }
+            set
+            {
+                foodName = value;
+            }
+        }
+    }
+}
---
+++ GridView/GridViewComboxColumnCustomValue/GridViewComboxColumnCustomValueVB/GridViewComboxColumnCustomValue/RadForm1.vb
@@ -0,0 +1,120 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports Telerik.WinControls.UI
+
+Namespace GridViewComboxColumnCustomValue
+	Partial Public Class RadForm1
+		Inherits Telerik.WinControls.UI.RadForm
+
+		Private data As New List(Of Food)()
+
+		Public Sub New()
+			InitializeComponent()
+			radGridView1.AutoGenerateColumns = False
+			AddHandler radGridView1.EditorRequired, AddressOf radGridView1_EditorRequired
+			AddHandler radGridView1.CreateCell, AddressOf radGridView1_CreateCell
+			data.Add(New Food(0, "Onion"))
+			data.Add(New Food(1, "Cucumber"))
+			data.Add(New Food(2, "Tomato"))
+			data.Add(New Food(3, "Peach"))
+			data.Add(New Food(4, "Banana"))
+			data.Add(New Food(5, "Grape"))
+
+			Dim comboBoxColumn As New GridViewComboBoxColumn()
+			comboBoxColumn.DataSource = data
+			comboBoxColumn.DisplayMember = "FoodName"
+			comboBoxColumn.ValueMember = "FoodName"
+			comboBoxColumn.Width = 200
+			comboBoxColumn.DropDownStyle = Telerik.WinControls.RadDropDownStyle.DropDown
+			radGridView1.Columns.Add(comboBoxColumn)
+
+			radGridView1.Rows.Add("Onion")
+
+			AddHandler radGridView1.CellValueChanged, AddressOf radGridView1_CellValueChanged
+		End Sub
+
+		Private Sub radGridView1_CreateCell(ByVal sender As Object, ByVal e As GridViewCreateCellEventArgs)
+			If e.CellType Is GetType(GridComboBoxCellElement) Then
+				e.CellElement = New MyCombBoxCellElement(TryCast(e.Column, GridViewDataColumn), e.Row)
+			End If
+		End Sub
+
+		Private Sub radGridView1_CellValueChanged(ByVal sender As Object, ByVal e As GridViewCellEventArgs)
+		End Sub
+
+		Private Sub radGridView1_EditorRequired(ByVal sender As Object, ByVal e As EditorRequiredEventArgs)
+			If e.EditorType Is GetType(RadDropDownListEditor) Then
+				e.EditorType = GetType(MyRadDropDownListEditor)
+			End If
+		End Sub
+	End Class
+
+	Public Class MyRadDropDownListEditor
+		Inherits RadDropDownListEditor
+
+		Public Overrides Property Value() As Object
+			Get
+				Dim editor As RadDropDownListElement = TryCast(Me.EditorElement, RadDropDownListElement)
+				If editor.SelectedItem IsNot Nothing Then
+					If Not String.IsNullOrEmpty(editor.ValueMember) Then
+						Return editor.SelectedItem.Value
+					End If
+
+					Return editor.SelectedItem.Text
+				End If
+
+				Return editor.Text
+			End Get
+			Set(ByVal value As Object)
+				MyBase.Value = value
+			End Set
+		End Property
+	End Class
+
+	Public Class MyCombBoxCellElement
+		Inherits GridComboBoxCellElement
+
+		Public Sub New(ByVal col As GridViewColumn, ByVal row As GridRowElement)
+			MyBase.New(col, row)
+		End Sub
+
+		Public Overrides Sub SetContent()
+
+			SetContentCore(Me.Value)
+		End Sub
+	End Class
+
+	Public Class Food
+        Private m_foodID As Integer
+
+        Private s_foodName As String
+
+        Public Sub New(ByVal foodID As Integer, ByVal foodName As String)
+            Me.m_foodID = foodID
+            Me.s_foodName = foodName
+        End Sub
+
+        Public Property FoodID() As Integer
+            Get
+                Return m_foodID
+            End Get
+            Set(ByVal value As Integer)
+                m_foodID = value
+            End Set
+        End Property
+
+        Public Property FoodName() As String
+            Get
+                Return s_foodName
+            End Get
+            Set(ByVal value As String)
+                s_foodName = value
+            End Set
+        End Property
+	End Class
+End Namespace
