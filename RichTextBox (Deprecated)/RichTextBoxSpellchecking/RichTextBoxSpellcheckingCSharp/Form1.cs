be0cb3fc1d7df4d36a22df06a2021bcaf9af0dde
Desislava Yordanova <dess.yordanova@gmail.com>
2021-06-02 11:11:21 +0300

    RichTextBoxSpellchecking

---
+++ RichTextBox (Deprecated)/RichTextBoxSpellchecking/RichTextBoxSpellcheckingCSharp/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls.RichTextBox;
+using Telerik.WinControls.RichTextBox.Layout;
+using Telerik.WinControls.RichTextBox.TextSearch;
+using Telerik.WinControls.UI;
+
+namespace RichTextBoxSpellchecking
+{
+    public partial class Form1 : RadForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            new SpellCheckRichTextBox()
+            {
+                Parent = this,
+                Dock = DockStyle.Fill
+            };
+        }
+    }
+
+    public class SpellCheckRichTextBox : RadRichTextBox
+    { 
+        #region Fields
+
+        private RadSpellChecker spellChecker;
+        private IControlSpellChecker controlSpellChecker;
+
+        #endregion
+
+        #region Properties
+
+        public new RadSpellChecker SpellChecker
+        {
+            get { return this.spellChecker; }
+        }
+
+        public new bool IsSpellCheckingEnabled
+        {
+            get { return true; }
+        }
+
+        public bool AutoReplaceOnSpellCheck { get; set; }
+
+        public IControlSpellChecker ControlSpellChecker
+        {
+            get { return this.controlSpellChecker; }
+        }
+
+        #endregion
+
+        #region SpellChecking
+
+        protected override void CreateChildItems(Telerik.WinControls.RadElement parent)
+        {
+            base.CreateChildItems(parent);
+
+            this.spellChecker = new RadSpellChecker();
+            this.controlSpellChecker = this.spellChecker.GetControlSpellChecker(typeof(RadRichTextBox));
+            this.controlSpellChecker.CurrentControl = this;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                this.SpellCheckCore(this.GetCurrentWord());
+            }
+
+            base.OnKeyDown(e);
+        }
+
+        protected override void OnLoad(System.Drawing.Size desiredSize)
+        {
+            base.OnLoad(desiredSize);
+            //this is need in order to initialize the dictionaries when loaded instead of when typing
+            this.controlSpellChecker.SpellChecker.CheckWordIsCorrect("loadFasterWord");
+        }
+
+        private string GetCurrentWord()
+        {
+            DocumentPosition endPosition = new DocumentPosition(this.Document.CaretPosition);
+            DocumentPosition startPosition = new DocumentPosition(endPosition);
+            this.SelectCurrentWord(startPosition);
+            string lastWord = this.Document.Selection.GetSelectedText();
+            this.Document.Selection.Clear();
+
+            return lastWord;
+        }
+
+        private void SelectCurrentWord(DocumentPosition position)
+        {
+            position.MoveToCurrentWordStart();
+            this.Document.Selection.SetSelectionStart(position);
+            position.MoveToCurrentWordEnd();
+            this.Document.Selection.AddSelectionEnd(position);
+        }
+
+        protected virtual void SpellCheckCore(string word)
+        {
+            if (string.IsNullOrEmpty(word) || this.controlSpellChecker.IgnoredWords.ContainsWord(word))
+            {
+                return;
+            }
+
+            this.SelectCurrentWord(this.Document.CaretPosition);
+
+            if (this.AutoReplaceOnSpellCheck)
+            {
+                string[] suggestions = (string[])this.ControlSpellChecker.SpellChecker.GetSuggestions(this.Document.Selection.GetSelectedText());
+                if (suggestions.Length > 0)
+                {
+                    this.Insert(suggestions[0]);
+                }
+            }
+            else
+            {
+                if (!controlSpellChecker.SpellChecker.CheckWordIsCorrect(word))
+                {
+                    this.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.Wave);
+                    this.Document.Selection.Clear();
+
+                    this.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.None);
+                }
+                else
+                {
+                    this.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.None);
+                    this.Document.Selection.Clear();
+                }
+            }
+        }
+
+        public void IgnoreWord(string word)
+        {
+            this.controlSpellChecker.IgnoredWords.AddWord(word);
+            DocumentTextSearch search = new DocumentTextSearch(this.Document);
+            DocumentPosition savedPosition = this.Document.CaretPosition;
+
+            foreach (TextRange text in search.FindAll(word))
+            {
+                this.Document.Selection.AddSelectionStart(text.StartPosition);
+                this.Document.Selection.AddSelectionEnd(text.EndPosition);
+                this.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.None);
+            }
+
+            this.Document.Selection.AddSelectionStart(savedPosition);
+            this.Document.Selection.AddSelectionEnd(savedPosition);
+        }
+
+        #endregion
+
+        #region SpellCheckContextMenu
+
+        private RadDropDownMenu BuildContextMenu(SpanLayoutBox spanBox)
+        {
+            RadDropDownMenu menu = new RadDropDownMenu();
+            RadMenuItem menuItem;
+
+            if (this.Document.Selection.IsEmpty && this.IsSpellCheckingEnabled && spanBox != null)
+            {
+                this.Document.CaretPosition.MoveToInline(spanBox, 0);
+
+                string spanBoxTextAlphaNumericOnly = String.Concat(spanBox.Text.TakeWhile(c => char.IsLetterOrDigit(c)));
+
+                if (spanBoxTextAlphaNumericOnly.Length > 0 && !this.ControlSpellChecker.SpellChecker.CheckWordIsCorrect(spanBoxTextAlphaNumericOnly))
+                {
+                    string[] suggestions = (string[])this.ControlSpellChecker.SpellChecker.GetSuggestions(spanBox.Text);
+
+                    if (suggestions.Length <= 0)
+                    {
+                        menuItem = new RadMenuItem("(No Spelling Suggestions)");
+                        menuItem.Enabled = false;
+                        menu.Items.Add(menuItem);
+                    }
+
+                    foreach (string suggestion in suggestions)
+                    {
+                        menuItem = new RadMenuItem(suggestion);
+                        menuItem.Click += (object sender, EventArgs e) => { this.ReplaceCurrentWord(spanBox, (sender as RadMenuItem).Text); };
+                        menu.Items.Add(menuItem);
+                    }
+
+                    menu.Items.Add(new SeparatorElement());
+
+                    menuItem = new RadMenuItem("Add to Dictionary");
+                    menuItem.Click += (object sender, EventArgs e) => { this.ControlSpellChecker.SpellChecker.AddWord(spanBoxTextAlphaNumericOnly); };
+                    menu.Items.Add(menuItem);
+
+                    menu.Items.Add(new SeparatorElement());
+                }
+            }
+
+            menuItem = new RadMenuItem("Undo");
+            menuItem.Click += (object sender, EventArgs e) => { this.Undo(); };
+            menu.Items.Add(menuItem);
+
+            menu.Items.Add(new SeparatorElement());
+
+            menuItem = new RadMenuItem("Cut");
+            menuItem.Click += (object sender, EventArgs e) => { this.Cut(); };
+            menu.Items.Add(menuItem);
+
+            menuItem = new RadMenuItem("Copy");
+            menuItem.Click += (object sender, EventArgs e) => { this.Copy(); };
+            menu.Items.Add(menuItem);
+
+            menuItem = new RadMenuItem("Paste");
+            menuItem.Click += (object sender, EventArgs e) => { this.Paste(); };
+
+            menu.Items.Add(menuItem);
+
+            menuItem = new RadMenuItem("Delete");
+            menuItem.Click += (object sender, EventArgs e) => { this.Delete(false); };
+            menu.Items.Add(menuItem);
+
+            menu.Items.Add(new SeparatorElement());
+
+            menuItem = new RadMenuItem("Select All");
+            menuItem.Click += (object sender, EventArgs e) => { this.Document.Selection.SelectAll(); };
+            menu.Items.Add(menuItem);
+
+            return menu;
+        }
+
+        private void ReplaceCurrentWord(SpanLayoutBox spanBox, string newWord)
+        {
+            this.Document.CaretPosition.MoveToInline(spanBox, 0);
+
+            DocumentPosition endPosition = new DocumentPosition(this.Document.CaretPosition);
+            endPosition.MoveToCurrentWordEnd();
+
+            this.Document.Selection.Clear();
+            this.Document.Selection.AddSelectionStart(this.Document.CaretPosition);
+            this.Document.Selection.AddSelectionEnd(endPosition);
+
+            this.Insert(newWord);
+        }
+
+        protected override void OnMouseDown(System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                SpanLayoutBox spanBox = this.Document.GetLayoutBoxByPosition(e.Location) as SpanLayoutBox;
+                this.BuildContextMenu(spanBox).Show(this, e.Location);
+            }
+
+            base.OnMouseDown(e);
+        }
+
+        #endregion
+    }
+}
---
+++ RichTextBox (Deprecated)/RichTextBoxSpellchecking/RichTextBoxSpellcheckingVB/Form1.vb
@@ -0,0 +1,264 @@
+﻿Imports Telerik.WinControls.RichTextBox
+Imports Telerik.WinControls.UI
+Imports Telerik.WinControls.RichTextBox.TextSearch
+Imports Telerik.WinControls.RichTextBox.Layout
+
+Public Class Form1
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        Dim rtb As New SpellCheckRichTextBox()
+
+        rtb.Parent = Me
+        rtb.Dock = DockStyle.Fill
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
+
+
+Public Class SpellCheckRichTextBox
+    Inherits RadRichTextBox
+#Region "Fields"
+
+    Private m_spellChecker As RadSpellChecker
+    Private m_controlSpellChecker As IControlSpellChecker
+
+#End Region
+
+#Region "Properties"
+
+    Public Shadows ReadOnly Property SpellChecker() As RadSpellChecker
+        Get
+            Return Me.m_spellChecker
+        End Get
+    End Property
+
+    Public Shadows ReadOnly Property IsSpellCheckingEnabled() As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Property AutoReplaceOnSpellCheck() As Boolean
+        Get
+            Return m_AutoReplaceOnSpellCheck
+        End Get
+        Set(value As Boolean)
+            m_AutoReplaceOnSpellCheck = value
+        End Set
+    End Property
+    Private m_AutoReplaceOnSpellCheck As Boolean
+
+    Public ReadOnly Property ControlSpellChecker() As IControlSpellChecker
+        Get
+            Return Me.m_controlSpellChecker
+        End Get
+    End Property
+
+#End Region
+
+#Region "SpellChecking"
+
+    Protected Overrides Sub CreateChildItems(parent As Telerik.WinControls.RadElement)
+        MyBase.CreateChildItems(parent)
+
+        Me.m_spellChecker = New RadSpellChecker()
+        Me.m_controlSpellChecker = Me.m_spellChecker.GetControlSpellChecker(GetType(RadRichTextBox))
+        Me.m_controlSpellChecker.CurrentControl = Me
+    End Sub
+
+    Protected Overrides Sub OnKeyDown(e As KeyEventArgs)
+        If e.KeyCode = Keys.Space Then
+            Me.SpellCheckCore(Me.GetCurrentWord())
+        End If
+
+        MyBase.OnKeyDown(e)
+    End Sub
+
+    Protected Overrides Sub OnLoad(desiredSize As System.Drawing.Size)
+        MyBase.OnLoad(desiredSize)
+        'this is need in order to initialize the dictionaries when loaded instead of when typing
+        Me.m_controlSpellChecker.SpellChecker.CheckWordIsCorrect("loadFasterWord")
+    End Sub
+
+    Private Function GetCurrentWord() As String
+        Dim endPosition As New DocumentPosition(Me.Document.CaretPosition)
+        Dim startPosition As New DocumentPosition(endPosition)
+        Me.SelectCurrentWord(startPosition)
+        Dim lastWord As String = Me.Document.Selection.GetSelectedText()
+        Me.Document.Selection.Clear()
+
+        Return lastWord
+    End Function
+
+    Private Sub SelectCurrentWord(position As DocumentPosition)
+        position.MoveToCurrentWordStart()
+        Me.Document.Selection.SetSelectionStart(position)
+        position.MoveToCurrentWordEnd()
+        Me.Document.Selection.AddSelectionEnd(position)
+    End Sub
+
+    Protected Overridable Sub SpellCheckCore(word As String)
+        If String.IsNullOrEmpty(word) OrElse Me.m_controlSpellChecker.IgnoredWords.ContainsWord(word) Then
+            Return
+        End If
+
+        Me.SelectCurrentWord(Me.Document.CaretPosition)
+
+        If Me.AutoReplaceOnSpellCheck Then
+            Dim suggestions As String() = DirectCast(Me.ControlSpellChecker.SpellChecker.GetSuggestions(Me.Document.Selection.GetSelectedText()), String())
+            If suggestions.Length > 0 Then
+                Me.Insert(suggestions(0))
+            End If
+        Else
+            If Not m_controlSpellChecker.SpellChecker.CheckWordIsCorrect(word) Then
+                Me.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.Wave)
+                Me.Document.Selection.Clear()
+
+                Me.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.None)
+            Else
+                Me.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.None)
+                Me.Document.Selection.Clear()
+            End If
+        End If
+    End Sub
+
+    Public Sub IgnoreWord(word As String)
+        Me.m_controlSpellChecker.IgnoredWords.AddWord(word)
+        Dim search As New DocumentTextSearch(Me.Document)
+        Dim savedPosition As DocumentPosition = Me.Document.CaretPosition
+
+        For Each text As TextRange In search.FindAll(word)
+            Me.Document.Selection.AddSelectionStart(text.StartPosition)
+            Me.Document.Selection.AddSelectionEnd(text.EndPosition)
+            Me.ChangeUnderlineDecoration(Telerik.WinControls.RichTextBox.UI.UnderlineType.None)
+        Next
+
+        Me.Document.Selection.AddSelectionStart(savedPosition)
+        Me.Document.Selection.AddSelectionEnd(savedPosition)
+    End Sub
+
+#End Region
+
+#Region "SpellCheckContextMenu"
+
+    Private Function BuildContextMenu(spanBox As SpanLayoutBox) As RadDropDownMenu
+        Dim menu As New RadDropDownMenu()
+        Dim menuItem As RadMenuItem
+
+        If Me.Document.Selection.IsEmpty AndAlso Me.IsSpellCheckingEnabled AndAlso spanBox IsNot Nothing Then
+            Me.Document.CaretPosition.MoveToInline(spanBox, 0)
+
+            Dim spanBoxTextAlphaNumericOnly As String = [String].Concat(spanBox.Text.TakeWhile(Function(c) Char.IsLetterOrDigit(c)))
+
+            If spanBoxTextAlphaNumericOnly.Length > 0 AndAlso Not Me.ControlSpellChecker.SpellChecker.CheckWordIsCorrect(spanBoxTextAlphaNumericOnly) Then
+                Dim suggestions As String() = DirectCast(Me.ControlSpellChecker.SpellChecker.GetSuggestions(spanBox.Text), String())
+
+                If suggestions.Length <= 0 Then
+                    menuItem = New RadMenuItem("(No Spelling Suggestions)")
+                    menuItem.Enabled = False
+                    menu.Items.Add(menuItem)
+                End If
+
+                For Each suggestion As String In suggestions
+                    menuItem = New RadMenuItem(suggestion)
+                    AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                                   Me.ReplaceCurrentWord(spanBox, TryCast(sender, RadMenuItem).Text)
+
+                                               End Sub
+                    menu.Items.Add(menuItem)
+                Next
+
+                menu.Items.Add(New SeparatorElement())
+
+                menuItem = New RadMenuItem("Add to Dictionary")
+                AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                               Me.ControlSpellChecker.SpellChecker.AddWord(spanBoxTextAlphaNumericOnly)
+
+                                           End Sub
+                menu.Items.Add(menuItem)
+
+                menu.Items.Add(New SeparatorElement())
+
+            End If
+        End If
+
+
+        menuItem = New RadMenuItem("Undo")
+        AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                       Me.Undo()
+
+                                   End Sub
+        menu.Items.Add(menuItem)
+
+        menu.Items.Add(New SeparatorElement())
+
+        menuItem = New RadMenuItem("Cut")
+        AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                       Me.Cut()
+
+                                   End Sub
+        menu.Items.Add(menuItem)
+
+        menuItem = New RadMenuItem("Copy")
+        AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                       Me.Copy()
+
+                                   End Sub
+        menu.Items.Add(menuItem)
+
+        menuItem = New RadMenuItem("Paste")
+        AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                       Me.Paste()
+
+                                   End Sub
+
+        menu.Items.Add(menuItem)
+
+        menuItem = New RadMenuItem("Delete")
+        AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                       Me.Delete(False)
+
+                                   End Sub
+        menu.Items.Add(menuItem)
+
+        menu.Items.Add(New SeparatorElement())
+
+        menuItem = New RadMenuItem("Select All")
+        AddHandler menuItem.Click, Sub(sender As Object, e As EventArgs)
+                                       Me.Document.Selection.SelectAll()
+
+                                   End Sub
+        menu.Items.Add(menuItem)
+
+        Return menu
+    End Function
+
+    Private Sub ReplaceCurrentWord(spanBox As SpanLayoutBox, newWord As String)
+        Me.Document.CaretPosition.MoveToInline(spanBox, 0)
+
+        Dim endPosition As New DocumentPosition(Me.Document.CaretPosition)
+        endPosition.MoveToCurrentWordEnd()
+
+        Me.Document.Selection.Clear()
+        Me.Document.Selection.AddSelectionStart(Me.Document.CaretPosition)
+        Me.Document.Selection.AddSelectionEnd(endPosition)
+
+        Me.Insert(newWord)
+    End Sub
+
+    Protected Overrides Sub OnMouseDown(e As System.Windows.Forms.MouseEventArgs)
+        If e.Button = System.Windows.Forms.MouseButtons.Right Then
+            Dim spanBox As SpanLayoutBox = TryCast(Me.Document.GetLayoutBoxByPosition(e.Location), SpanLayoutBox)
+            Me.BuildContextMenu(spanBox).Show(Me, e.Location)
+        End If
+
+        MyBase.OnMouseDown(e)
+    End Sub
+
+#End Region
+End Class
