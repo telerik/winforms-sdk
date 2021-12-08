ac9d67b371bf45af026169362943110d20ceb684
Desislava Yordanova <dess.yordanova@gmail.com>
2021-12-08 14:19:49 +0200

    Updated Map SDK for pins with images

--- Map/RadMapCustomPins/RadMapCustomPins/RadMapCustomPinForm.cs
+++ Map/RadMapCustomPins/RadMapCustomPins/RadMapCustomPinForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -37,6 +38,8 @@ namespace RadMapCustomPins
         private void SetupProviders()
         {
             OpenStreetMapProvider osmProvider = new OpenStreetMapProvider();
+            MapTileDownloader tileDownloader = osmProvider.TileDownloader as MapTileDownloader;
+            tileDownloader.WebHeaders.Add(System.Net.HttpRequestHeader.UserAgent, "your application name");
             this.radMap1.MapElement.Providers.Add(osmProvider);
         }
 
@@ -50,12 +53,10 @@ namespace RadMapCustomPins
         private PointL pixelLocation;
         private RectangleL drawRect;
         private bool isImageInViewPort;
-
         public CustomMapPin(PointG location)
             : base(location)
         {
         }
-
         public Image Image
         {
             get
@@ -67,34 +68,36 @@ namespace RadMapCustomPins
                 this.image = value;
             }
         }
-
         public override bool IsInViewport
         {
             get { return this.Image != null ? this.isImageInViewPort : base.IsInViewport; }
         }
-
         public override void Paint(IGraphics graphics, IMapViewport viewport)
         {
-            if (this.Image == null)
-            {
-                base.Paint(graphics, viewport);
-                return;
-            }
-
             object state = graphics.SaveState();
-            graphics.TranslateTransform(drawRect.X, drawRect.Y);
-            Graphics g = graphics.UnderlayGraphics as Graphics;
-
+            graphics.ChangeSmoothingMode(SmoothingMode.AntiAlias);
+            MapVisualElementInfo info = this.GetVisualElementInfo(viewport);
+            GraphicsPath path = info.Path.Clone() as GraphicsPath;
+            GraphicsPath dotPath = new GraphicsPath();
             long mapSize = MapTileSystemHelper.MapSize(viewport.ZoomLevel);
-
-            for (int i = -1; i <= viewport.NumberOfWraparounds; i++)
+            Matrix matrixOffset = new Matrix();
+            matrixOffset.Translate(viewport.PanOffset.Width + info.Offset.X, viewport.PanOffset.Height + info.Offset.Y);
+            path.Transform(matrixOffset);
+            Matrix matrixWraparound = new Matrix();
+            matrixWraparound.Translate(mapSize, 0);
+            for (int i = 0; i < viewport.NumberOfWraparounds; i++)
             {
-                g.DrawImage(this.Image, new RectangleF(i * mapSize, 0, this.Image.Size.Width, this.Image.Size.Height));
+                RectangleF bounds = path.GetBounds();
+                float diameter = bounds.Width / 3F;
+                dotPath.AddEllipse(bounds.X + diameter, bounds.Y + diameter, diameter, diameter);
+                graphics.FillPath(this.BorderColor, dotPath);
+                //draw the image
+                Point imageLocation = new Point((int)bounds.Location.X + (int)bounds.Width / 2 - this.image.Width / 2, (int)bounds.Location.Y);
+                graphics.DrawImage(imageLocation, this.Image, true);
+                path.Transform(matrixWraparound);
             }
-
             graphics.RestoreState(state);
         }
-
         public override void ViewportChanged(IMapViewport viewport, ViewportChangeAction action)
         {
             if (this.Image == null)
@@ -102,46 +105,36 @@ namespace RadMapCustomPins
                 base.ViewportChanged(viewport, action);
                 return;
             }
-
             long mapSize = MapTileSystemHelper.MapSize(viewport.ZoomLevel);
-
             if ((action & ViewportChangeAction.Zoom) != 0)
             {
                 this.pixelLocation = MapTileSystemHelper.LatLongToPixelXY(this.Location, viewport.ZoomLevel);
             }
-
             if ((action & ViewportChangeAction.Pan) != 0)
             {
                 this.drawRect = new RectangleL(pixelLocation.X - this.Image.Size.Width / 2, pixelLocation.Y - this.Image.Size.Height, this.Image.Size.Width, this.Image.Size.Height);
             }
-
             RectangleL wraparoundDrawRect = this.drawRect;
-
             for (int i = 0; i <= viewport.NumberOfWraparounds; i++)
             {
                 if (wraparoundDrawRect.IntersectsWith(viewport.ViewportInPixels))
                 {
                     this.isImageInViewPort = true;
-
                     break;
                 }
-
                 wraparoundDrawRect.Offset(mapSize, 0L);
             }
-
             if (!this.IsInViewport)
             {
                 return;
             }
         }
-
         public override bool HitTest(PointG pointG, PointL pointL, IMapViewport viewport)
         {
             if (this.Image == null)
             {
                 return base.HitTest(pointG, pointL, viewport);
             }
-
             return this.drawRect.Contains(pointL);
         }
     }
--- Map/RadMapCustomPins/RadMapCustomPinsVB/RadMapCustomPinForm.vb
+++ Map/RadMapCustomPins/RadMapCustomPinsVB/RadMapCustomPinForm.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
 Imports Telerik.WinControls.Paint
 Imports Telerik.WinControls.UI
 Imports Telerik.WinControls.UI.Map
@@ -19,7 +20,9 @@ Public Class RadMapCustomPinForm
     End Sub
 
     Private Sub SetupProviders()
-        Dim osmProvider As OpenStreetMapProvider = New OpenStreetMapProvider()
+        Dim osmProvider As New OpenStreetMapProvider()
+        Dim tileDownloader As MapTileDownloader = TryCast(osmProvider.TileDownloader, MapTileDownloader)
+        tileDownloader.WebHeaders.Add(System.Net.HttpRequestHeader.UserAgent, "your application name")
         Me.radMap1.MapElement.Providers.Add(osmProvider)
     End Sub
 End Class
@@ -28,87 +31,77 @@ End Class
 '#region CustomMapPin
 Public Class CustomMapPin
     Inherits MapPin
-
     Private _image As Image
-
     Private pixelLocation As PointL
-
     Private drawRect As RectangleL
-
     Private isImageInViewPort As Boolean
-
     Public Sub New(ByVal location As PointG)
         MyBase.New(location)
     End Sub
-
     Public Property Image As Image
         Get
             Return _image
         End Get
-
         Set(ByVal value As Image)
             Me._image = value
         End Set
     End Property
-
     Public Overrides ReadOnly Property IsInViewport As Boolean
         Get
             Return If(Me.Image IsNot Nothing, Me.isImageInViewPort, MyBase.IsInViewport)
         End Get
     End Property
-
     Public Overrides Sub Paint(ByVal graphics As IGraphics, ByVal viewport As IMapViewport)
-        If Me.Image Is Nothing Then
-            MyBase.Paint(graphics, viewport)
-            Return
-        End If
-
         Dim state As Object = graphics.SaveState()
-        graphics.TranslateTransform(drawRect.X, drawRect.Y)
-        Dim g As Graphics = TryCast(graphics.UnderlayGraphics, Graphics)
+        graphics.ChangeSmoothingMode(SmoothingMode.AntiAlias)
+        Dim info As MapVisualElementInfo = Me.GetVisualElementInfo(viewport)
+        Dim path As GraphicsPath = TryCast(info.Path.Clone(), GraphicsPath)
+        Dim dotPath As GraphicsPath = New GraphicsPath()
         Dim mapSize As Long = MapTileSystemHelper.MapSize(viewport.ZoomLevel)
-        For i As Integer = -1 To viewport.NumberOfWraparounds
-            g.DrawImage(Me.Image, New RectangleF(i * mapSize, 0, Me.Image.Size.Width, Me.Image.Size.Height))
+        Dim matrixOffset As Matrix = New Matrix()
+        matrixOffset.Translate(viewport.PanOffset.Width + info.Offset.X, viewport.PanOffset.Height + info.Offset.Y)
+        path.Transform(matrixOffset)
+        Dim matrixWraparound As Matrix = New Matrix()
+        matrixWraparound.Translate(mapSize, 0)
+        For i As Integer = 0 To viewport.NumberOfWraparounds - 1
+            Dim bounds As RectangleF = path.GetBounds()
+            Dim diameter As Single = bounds.Width / 3.0F
+            dotPath.AddEllipse(bounds.X + diameter, bounds.Y + diameter, diameter, diameter)
+            graphics.FillPath(Me.BorderColor, dotPath)
+            Dim imageLocation As Point = New Point(CInt(bounds.Location.X) + CInt(bounds.Width) / 2 - Me.Image.Width / 2, CInt(bounds.Location.Y))
+            graphics.DrawImage(imageLocation, Me.Image, True)
+            path.Transform(matrixWraparound)
         Next
-
         graphics.RestoreState(state)
     End Sub
-
     Public Overrides Sub ViewportChanged(ByVal viewport As IMapViewport, ByVal action As ViewportChangeAction)
         If Me.Image Is Nothing Then
             MyBase.ViewportChanged(viewport, action)
             Return
         End If
-
         Dim mapSize As Long = MapTileSystemHelper.MapSize(viewport.ZoomLevel)
         If (action And ViewportChangeAction.Zoom) <> 0 Then
             Me.pixelLocation = MapTileSystemHelper.LatLongToPixelXY(Me.Location, viewport.ZoomLevel)
         End If
-
         If (action And ViewportChangeAction.Pan) <> 0 Then
             Me.drawRect = New RectangleL(pixelLocation.X - Me.Image.Size.Width / 2, pixelLocation.Y - Me.Image.Size.Height, Me.Image.Size.Width, Me.Image.Size.Height)
         End If
-
         Dim wraparoundDrawRect As RectangleL = Me.drawRect
         For i As Integer = 0 To viewport.NumberOfWraparounds
             If wraparoundDrawRect.IntersectsWith(viewport.ViewportInPixels) Then
                 Me.isImageInViewPort = True
                 Exit For
             End If
-
             wraparoundDrawRect.Offset(mapSize, 0L)
         Next
-
         If Not Me.IsInViewport Then
             Return
         End If
     End Sub
-
     Public Overrides Function HitTest(ByVal pointG As PointG, ByVal pointL As PointL, ByVal viewport As IMapViewport) As Boolean
         If Me.Image Is Nothing Then
             Return MyBase.HitTest(pointG, pointL, viewport)
         End If
-
         Return Me.drawRect.Contains(pointL)
     End Function
 End Class
