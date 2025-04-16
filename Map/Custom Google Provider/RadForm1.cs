38e0d8685617439e4afc1ad66ad638c7e3b4d9ef
Dinko Krastev <dinko.krastev@progress.com>
2025-04-16 15:00:29 +0300

    Add Custom Google Provider

---
+++ Map/Custom Google Provider/RadForm1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace RadMapCustomGoogleProvider
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+            this.radMap1.ShowSearchBar = false;
+            IMapProvider provider = new GoogleCustomProvider();
+            this.radMap1.Providers.Add(provider);
+        }
+    }
+}
