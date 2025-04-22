093f82e1c5299053ff055dd6ec2afa5adfae6d2e
Nadya Todorova <karaivanova.nadya@gmail.com>
2025-04-22 17:30:21 +0300

    New SDK projects: Custom theme as default

---
+++ Themes/CustomThemeAsDefault/CustomTheme-net48/CustomTheme-net48/RadForm1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CustomTheme_net48
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radAIPrompt1 = new Telerik.WinControls.UI.RadAIPrompt();
+            this.windows11LightBlue1 = new Windows11LightBlueClassLibrary.Windows11LightBlue();
+            ((System.ComponentModel.ISupportInitialize)(this.radAIPrompt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radAIPrompt1
+            // 
+            this.radAIPrompt1.Location = new System.Drawing.Point(40, 16);
+            this.radAIPrompt1.Margin = new System.Windows.Forms.Padding(5);
+            this.radAIPrompt1.Name = "radAIPrompt1";
+            this.radAIPrompt1.Size = new System.Drawing.Size(275, 305);
+            this.radAIPrompt1.TabIndex = 0;
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 354);
+            this.Controls.Add(this.radAIPrompt1);
+            this.Name = "RadForm1";
+            this.Text = "RadForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.radAIPrompt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Telerik.WinControls.UI.RadAIPrompt radAIPrompt1;
+        private Windows11LightBlueClassLibrary.Windows11LightBlue windows11LightBlue1;
+    }
+}
---
+++ Themes/CustomThemeAsDefault/CustomTheme-net48/Windows11LightBlueClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windows11LightBlueClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Progress")]
+[assembly: AssemblyProduct("Windows11LightBlueClassLibrary")]
+[assembly: AssemblyCopyright("Copyright © Progress 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0cd12c38-0825-4485-ac4d-e056983fcbb8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
